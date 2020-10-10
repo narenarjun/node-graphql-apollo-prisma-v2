74de1c97fcda66a161c77e7d2ae019bf0806ed29
Narendran <narendev0610@gmail.com>
2020-10-11 02:07:26 +0530

    added prisma2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer } from "apollo-server";
 
 import { loadSchemaSync } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
@@ -7,12 +7,6 @@ import { addResolversToSchema } from "@graphql-tools/schema";
 import { join } from "path";
 
 const Port = process.env.PORT || 4332;
-
-// const typeDefs = gql`
-//   type Query {
-//     greet(query: String): String!
-//   }
-// `;
 
 const weirdname = loadSchemaSync(join(__dirname, "schema.graphql"), {
   loaders: [new GraphQLFileLoader()],
@@ -33,15 +27,11 @@ const resolvers = {
 
 const schemawithresolver = addResolversToSchema({
   schema: weirdname,
-  //   schema,
   resolvers,
 });
 
-// console.log(`is this logged ? ${typedefwithresolver}`);
-
 const server = new ApolloServer({
   schema: schemawithresolver,
-  //   resolvers,
 });
 
 server.listen({ port: Port }).then(({ url }) => {
