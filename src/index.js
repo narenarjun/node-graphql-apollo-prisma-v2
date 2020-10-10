705f520733c7cd4dd0ca40f2a833a16cbf43f21b
Narendran <narendev0610@gmail.com>
2020-10-11 00:14:56 +0530

    converted to do SDL first graphql server, typedef & resolvers merged , env variable added for port

--- src/index.js
+++ src/index.js
@@ -1,29 +1,49 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const typeDefs = gql`
-  type Query {
-    greet(query: String): String!
-  }
-`;
+import { loadSchemaSync } from "@graphql-tools/load";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { addResolversToSchema } from "@graphql-tools/schema";
+
+import { join } from "path";
+
+const Port = process.env.PORT || 4332;
+
+// const typeDefs = gql`
+//   type Query {
+//     greet(query: String): String!
+//   }
+// `;
+
+const weirdname = loadSchemaSync(join(__dirname, "schema.graphql"), {
+  loaders: [new GraphQLFileLoader()],
+});
 
 const resolvers = {
   Query: {
     greet(parent, args, ctx, info) {
       //   console.log(args.query); //! logging the user input
-      if (args.query !== null) {
-        return `hello ${args.query} !`;
+      if (args.query == null) {
+        return "hello world !!";
       } else {
-        return "hello world!";
+        return `hello ${args.query} !`;
       }
     },
   },
 };
 
-const server = new ApolloServer({
-  typeDefs,
+const schemawithresolver = addResolversToSchema({
+  schema: weirdname,
+  //   schema,
   resolvers,
 });
 
-server.listen({ port: 4221 }).then(({ url }) => {
+// console.log(`is this logged ? ${typedefwithresolver}`);
+
+const server = new ApolloServer({
+  schema: schemawithresolver,
+  //   resolvers,
+});
+
+server.listen({ port: Port }).then(({ url }) => {
   console.log(`Server is running at ${url} 🚀🚀🚀 `);
 });
